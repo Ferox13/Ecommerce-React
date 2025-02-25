d444d3a75893d46345dadcf1b8dab026f1eacfc1
Fernando Rodríguez Rodríguez <26763307+Ferox13@users.noreply.github.com>
2025-02-25 21:05:33 +0100

    Update main

--- ecommerce-react/src/main.tsx
+++ ecommerce-react/src/main.tsx
@@ -1,12 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ProductProvider } from "./context/ProductContext.js";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+      <ProductProvider>
+        <App />
+      </ProductProvider>
+);
