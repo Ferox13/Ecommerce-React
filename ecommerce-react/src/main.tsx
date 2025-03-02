1eec279b12ee454c5f36ce29c837499f259a6848
Fernando Rodríguez Rodríguez <26763307+Ferox13@users.noreply.github.com>
2025-03-02 13:44:20 +0100

    Update main.tsx

--- ecommerce-react/src/main.tsx
+++ ecommerce-react/src/main.tsx
@@ -4,13 +4,16 @@ import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ProductProvider } from "./context/ProductContext.js";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { CartProvider } from './context/CartContext.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
+    <CartProvider>
       <ProductProvider>
         <App />
       </ProductProvider>
+    </CartProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
