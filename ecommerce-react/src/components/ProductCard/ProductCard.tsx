94b0bbe7a09297b4e23198e39abc6151f932890c
Fernando Rodríguez Rodríguez <26763307+Ferox13@users.noreply.github.com>
2025-03-02 13:44:08 +0100

    Updated product card for add to cart

--- ecommerce-react/src/components/ProductCard/ProductCard.tsx
+++ ecommerce-react/src/components/ProductCard/ProductCard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Product } from "../../types";
-// import { useCart } from "../../context/CartContext";
+import { useCart } from "../../context/CartContext";
 import "./ProductCard.css";
+import { toast } from "react-toastify";
 
 interface ProductCardProps {
   product: Product;
@@ -10,7 +11,14 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product, customButton }) => {
   // const { addProduct } = useCart();
+  const { addProduct } = useCart();
 
+  const handleAddToCart = () => {
+    if (product) {
+      addProduct(product);
+      toast.success("Producto añadido al carrito");
+    }
+  };
   return (
     <div className="card customCard">
       {/* Imagen del producto */}
@@ -20,23 +28,19 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, customButton }) => {
         className="card-img-top"
         style={{ objectFit: "cover", height: "180px" }}
       />
-      
+
       {/* Cuerpo de la tarjeta */}
       <div className="card-body d-flex flex-column p-2">
-        <h6 className="card-title mb-1">
-          {product.title}
-        </h6>
-        <p className="card-text mb-2">
-          ${product.price}
-        </p>
-        
+        <h6 className="card-title mb-1">{product.title}</h6>
+        <p className="card-text mb-2">${product.price}</p>
+
         {/* Renderizado condicional del botón */}
         {customButton ? (
           customButton
         ) : (
           <button
             className="btn btn-success btn-sm mt-auto"
-            // onClick={() => addProduct(product)}
+            onClick={() => handleAddToCart()}
           >
             Añadir al carro
           </button>
