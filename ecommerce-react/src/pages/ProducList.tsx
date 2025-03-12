9335034601b4487d596c752188814e93e7fb5225
Fernando Rodríguez Rodríguez <26763307+Ferox13@users.noreply.github.com>
2025-03-12 21:09:19 +0100

    Nuevos estilos cards para todos los productos

--- ecommerce-react/src/pages/ProducList.tsx
+++ ecommerce-react/src/pages/ProducList.tsx
@@ -32,22 +32,28 @@ const ProductList: React.FC = () => {
 
   return (
     <div className="container">
-      <h1 className="mb-4 text-center">Todos los Productos</h1>
+      <h1 className="mb-4 text-center text-white">Todos los Productos</h1>
       <div className="row justify-content-center align-items-center">
         {products.map((product) => (
-          <div key={product.id} className="col-12 col-md-4 mb-4 d-flex justify-content-center align-items-center flex-wrap">
-            <ProductCard
-              product={product}
-              customButton={
-                <button
-                  className="btn btn-outline-secondary border-0 d-flex align-items-center gap-2"
-                  onClick={() => navigate(`/product/${product.id}`)}
-                  aria-label="Ver detalles"
-                >
-                  <Eye size={18} /> <span className="small">Detalles</span>
-                </button>
-              }
-            />
+          <div 
+            key={product.id} 
+            className="col-12 col-md-6 col-lg-6 mb-5 d-flex justify-content-center align-items-center"
+            style={{ maxWidth: "500px" }}
+          >
+            <div className="w-100 product-card-wrapper">
+              <ProductCard
+                product={product}
+                customButton={
+                  <button
+                    className="btn btn-outline-secondary border-0 d-flex align-items-center justify-content-center gap-2"
+                    onClick={() => navigate(`/product/${product.id}`)}
+                    aria-label="Ver detalles"
+                  >
+                    <Eye size={18} /> <span className="small">Detalles</span>
+                  </button>
+                }
+              />
+            </div>
           </div>
         ))}
       </div>
